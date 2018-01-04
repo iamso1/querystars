6e43d8005a266516681ef0ed5509834f5e5e7e63
Hsinhan <bmhiamso1@gmail.com>
2018-01-04 22:29:43 +0800

    1. 加入next的ignore 2. 調整專案架構

--- pages/learn.js
+++ pages/learn.js
@@ -1,5 +1,5 @@
-import Layout from '../components/layout';
-import Header from '../components/header';
+import Layout from '../src/components/layout';
+import Header from '../src/components/header';
 import Querystar from '../components/querystar/querystar';
 
 export default () => (
